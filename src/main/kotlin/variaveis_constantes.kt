31c5172f22eee1b38798064601218b63c8a8d7d1
= <kevinlucas.tec.info@gmail.com>
2021-01-28 12:41:09 -0300

    Variáveis Mutáveis e imutáveis

---
+++ src/main/kotlin/variaveis_constantes.kt
@@ -0,0 +1,19 @@
+fun main() {
+    // Variáveis multáveis
+    var nome = "Fulano"
+    println(nome)
+    nome = "Silva"
+    println(nome)
+
+    val largura = 5.4
+    println(largura)
+    //largura = 4.5 // não é possível alterar o valor
+
+    // Inicialização
+    //val y
+    val x: Int
+    x = 10
+    println(x)
+
+    
+}
