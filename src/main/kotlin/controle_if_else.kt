11c934fc5ad8f0e9e8bd2a27a2874fd265a8f86c
= <kevinlucas.tec.info@gmail.com>
2021-02-04 14:49:50 -0300

    Controle de fluxo - if else - Parte 1

---
+++ src/main/kotlin/controle_if_else.kt
@@ -0,0 +1,72 @@
+fun maiorDeIdade(idade: Int) {
+    // if else
+    if (idade > 18) {
+        println("Maior de idade")
+    } else {
+        println("Não é maior de idade")
+    }
+}
+
+fun maiorDeIdadeComRetorno(idade: Int): Boolean {
+    // if else
+    if (idade > 18) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// Com o retorno do tipo Boolean posso simplificar a lógica
+fun maiorDeIdadeComRetornoSimplificado(idade: Int): Boolean {
+    // if else
+    return idade > 18
+}
+
+fun calculaBonus(cargo: String, salario: Double): Double {
+    // Gerente Junior, Gerente Senior, Coordenador
+
+    var retorno = salario
+
+    if (cargo == "Coordenador") {
+        return salario * 0.2
+    } else {
+        if (cargo.contains("Gerente")) {
+            if (cargo == "Gerente Senior") {
+                return salario * 2
+            } else {
+                return salario * 0.5
+            }
+        }
+    }
+
+    return salario
+}
+
+fun calculaBonus2(cargo: String, salario: Double): Double {
+    // Gerente Junior, Gerente Senior, Coordenador
+
+    val bonus: Double
+
+    if (cargo == "Coordenador") {
+        bonus = salario * 0.2
+    } else if (cargo == "Gerente Junior") {
+        bonus = salario * 0.5
+    } else {
+        bonus = salario * 2
+    }
+
+    return bonus
+}
+
+fun main() {
+
+    maiorDeIdade(10)
+    println(maiorDeIdadeComRetorno(10))
+    println(maiorDeIdadeComRetornoSimplificado(10))
+
+    println(calculaBonus("Gerente Senior", 5000.0))
+    println(calculaBonus("Gerente Junior", 5000.0))
+    println(calculaBonus("Coordenador", 5000.0))
+
+    println(calculaBonus2("Programador", 5000.0))
+}
